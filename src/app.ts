5d2306644ba11e361a4f48cf2c180053f2c9a366
amabellaaguiluz-fullsuite <amabella.aguiluz@getfullsuite.com>
2026-03-10 08:21:18 +0800

    FEAT: API limiter
    API calls are protected by an API limiter that accepts a max of 5 requests per 15 minutes. This is to protect brute force attempts.

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import apiLimiter from "./middlewares/apiLimiter";
 
 import authRouter from "./routes/auth.routes";
 import notesRouter from "./routes/notes.routes";
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(apiLimiter);
 
 app.use("/auth", authRouter);   
 app.use("/notes", notesRouter);
